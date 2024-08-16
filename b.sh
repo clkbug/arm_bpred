bc84e436e272eda1ef89b84d32ee39669aa462f2
clkbug <mail@clkbug.net>
2024-08-16 20:32:34 +0900

    add raspi4 data

--- b.sh
+++ b.sh
@@ -8,6 +8,6 @@ for i in $(seq 1 64); do
   j=$((2 ** 15 / i))
   python3 b.py ${i} ${j} ${n} >/tmp/a.s
   gcc /tmp/a.s
-  time ./a.out
+  perf stat -ddd -x'\t' -o perf.inner${i}.outer${j}.nop${n}.tsv ./a.out
 done
 done
---
+++ b_stat.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import sys
+import glob
+import re
+
+def get(f, x):
+    m = re.search(x + "(\\d+)", f)
+    assert m is not None
+    return int(m.groups()[0])
+
+a = []
+for f in glob.glob(f"{sys.argv[1]}/*.tsv"):
+    with open(f) as fp:
+        for l in fp:
+            if "branch-misses" in l:
+                l = l.strip()
+                w = l.split("\t")
+                bpredmiss = int(w[0])
+                inner = get(f, "inner")
+                nop = get(f, "nop")
+                a.append((inner, nop, bpredmiss))
+
+a.sort()
+
+for x in a:
+    print(*x, sep="\t")
+    
