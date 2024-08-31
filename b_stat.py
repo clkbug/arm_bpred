56f58831833367282553115813ee4f4f87c2c4a7
clkbug <mail@clkbug.net>
2024-08-31 16:13:01 +0900

    update

--- b_stat.py
+++ b_stat.py
@@ -3,6 +3,7 @@
 import sys
 import glob
 import re
+import pandas
 
 
 def get(f, x):
@@ -11,19 +12,40 @@ def get(f, x):
     return int(m.groups()[0])
 
 
+def branch_addr(filename):
+    ret = {}
+    with open(filename) as fp:
+        curFun = ""
+        for l in fp:
+            # print(curFun)
+            if ">:" in l:
+                curFun = l.split("<")[1].replace(">:", "").strip()
+                continue
+            if curFun == ".loop_inner" and "cbnz" in l:
+                ret["br1"] = int(l.strip().split(":")[0], base=16)
+            if curFun == ".loop_outer" and "cbnz" in l:
+                ret["br2"] = int(l.strip().split(":")[0], base=16)
+    return ret
+
+
 a = []
 for f in glob.glob(f"{sys.argv[1]}/*.tsv"):
+    asm = "asm/" + f.split("/")[1].replace("perf.",
+                                           "").replace(".tsv", ".objdump")
+    data = branch_addr(asm)
     with open(f) as fp:
-        for l in fp:
-            if "branch-misses" in l:
-                l = l.strip()
-                w = l.split("\t")
-                bpredmiss = int(w[0])
-                inner = get(f, "inner")
-                nop = get(f, "nop")
-                a.append((inner, nop, bpredmiss))
+        data["inner"] = get(f, "inner")
+        data["outer"] = get(f, "outer")
+        data["nop"] = get(f, "nop")
+        data["nop2"] = get(f, "nop2")
+        for i, l in enumerate(fp):
+            if i < 2:
+                continue
+            w = l.split("\t")
+            k = w[2].replace(":u", "").replace("-", "_")
+            v = w[0] if w[0] != "<not supported>" else "NA"
+            data[k] = v
+        a.append(data)
 
-a.sort()
-
-for x in a:
-    print(*x, sep="\t")
+df = pandas.DataFrame.from_records(a)
+df.to_csv(sys.stdout, sep="\t", index=False)
