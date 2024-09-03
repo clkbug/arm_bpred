e99b53f6bb36bf4efee6839f0bdd22e831c68fc6
clkbug <mail@clkbug.net>
2024-09-03 22:16:38 +0900

    update run script

--- b.sh
+++ b.sh
@@ -5,11 +5,12 @@ set -x
 
 for n in $(seq 0 8); do
   for n2 in $(seq 0 8); do
-    for i in $(seq 65 128); do
+    for i in $(seq 1 128); do
       j=$((2 ** 15 / i))
       python3 b.py ${i} ${j} ${n} ${n2} >/tmp/a.s
       gcc /tmp/a.s b.c
-      perf stat -ddd -x'\t' -o perf.inner${i}.outer${j}.nop${n}.nop2${n2}.tsv ./a.out
+      perf stat -ddd -x'\t' -o perf.inner${i}.outer${j}.nop${n}.nop2${n2}.tsv ./a.out >inner${i}.outer${j}.nop${n}.nop2${n2}.stdout
+      objdump -d a.out >inner${i}.outer${j}.nop${n}.nop2${n2}.objdump
     done
   done
 done
